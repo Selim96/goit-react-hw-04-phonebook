97565fd4a214594636649bf15f8e85446eae83a2
Selim Fazylov <selim.fazylov1@gmail.com>
2022-04-22 14:13:07 +0300

    form updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import ContactList from './ContactList';
@@ -6,76 +6,118 @@ import { nanoid } from 'nanoid';
 // импорт тестового списка контактов
 // import defaultData from './data/data';
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  }
+function App() {
+  const [contacts, setContacts] = useState(() => (JSON.parse(window.localStorage.getItem('contacts')) ?? []));
 
-  componentDidMount() {
-    // запись тестового списка в локал сторедж
-    // localStorage.setItem('contacts', JSON.stringify(defaultData));
+  const [filter, setFilter] = useState('');
 
-    const savedContacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(savedContacts);
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-    if (parsedContacts) {
-      return this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      console.log('updated!!!!!!');
-
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  formSubmitData = (data) => {
+  const formSubmitData = (data) => {
     const objName = {
         id: nanoid(),
         name: data.name,
         number: data.number,
         }
-    
-    this.setState(p => (
-      { contacts: [objName, ...p.contacts], }));
+    setContacts(prev => [objName, ...prev]);
   }
 
-  filterChange = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const filterChange = e => {
+    setFilter(e.currentTarget.value);
   }
 
-  toDoFilter = () => {
-    const { contacts, filter } = this.state;
+  const toDoFilter = () => {
     return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
   }
 
-  checkFunction = (name) => {
-    return this.state.contacts.some(v => v.name.toLowerCase() === name.toLowerCase()); 
-  }
+  const checkFunction = (name) => (contacts.some(v => v.name.toLowerCase() === name.toLowerCase())); 
 
-  toDeleteContact = (id) => {
-    this.setState(p => ({
-      contacts: p.contacts.filter(e => e.id !== id)
-    }))
+  const toDeleteContact = (id) => {
+    setContacts(prev => (prev.filter(e => e.id !== id)))
   };
-
-  render() {
-    const contactsArr = this.toDoFilter();
-    
-    return (
-      <div>
+  
+  return <div>
         <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.formSubmitData} check={this.checkFunction} />
+        <ContactForm onSubmit={formSubmitData} check={checkFunction} />
         <h2>Contacts</h2>
-        <Filter value={this.state.filter} onChange={this.filterChange}/>
-        <ContactList contacts={contactsArr} toDelete={this.toDeleteContact}/>
+        <Filter value={filter} onChange={filterChange}/>
+        <ContactList contacts={toDoFilter()} toDelete={toDeleteContact}/>
     </div>
-    )
+}
+
+// ===============================================
+// class App extends Component {
+//   state = {
+//     contacts: [],
+//     filter: '',
+//   }
+
+//   componentDidMount() {
+//     // запись тестового списка в локал сторедж
+//     // localStorage.setItem('contacts', JSON.stringify(defaultData));
+
+//     const savedContacts = localStorage.getItem('contacts');
+//     const parsedContacts = JSON.parse(savedContacts);
+
+//     if (parsedContacts) {
+//       return this.setState({ contacts: parsedContacts });
+//     }
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (this.state.contacts !== prevState.contacts) {
+//       console.log('updated!!!!!!');
+
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+//   formSubmitData = (data) => {
+//     const objName = {
+//         id: nanoid(),
+//         name: data.name,
+//         number: data.number,
+//         }
     
-  };
-};
+//     this.setState(p => (
+//       { contacts: [objName, ...p.contacts], }));
+//   }
+
+//   filterChange = e => {
+//     this.setState({ filter: e.currentTarget.value });
+//   }
+
+//   toDoFilter = () => {
+//     const { contacts, filter } = this.state;
+//     return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
+//   }
+
+//   checkFunction = (name) => {
+//     return this.state.contacts.some(v => v.name.toLowerCase() === name.toLowerCase()); 
+//   }
+
+//   toDeleteContact = (id) => {
+//     this.setState(p => ({
+//       contacts: p.contacts.filter(e => e.id !== id)
+//     }))
+//   };
+
+//   render() {
+//     const contactsArr = this.toDoFilter();
+    
+//     return (
+//       <div>
+//         <h1>Phonebook</h1>
+//         <ContactForm onSubmit={this.formSubmitData} check={this.checkFunction} />
+//         <h2>Contacts</h2>
+//         <Filter value={this.state.filter} onChange={this.filterChange}/>
+//         <ContactList contacts={contactsArr} toDelete={this.toDeleteContact}/>
+//     </div>
+//     )
+    
+//   };
+// };
 
 export default App;
